77a087258ddfdffdb38eb791637a3a4e8c4d860a
jeiismael <jeiismael@gmail.com>
2023-11-13 21:44:55 +0800

    updated middleware to use both custom and cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,13 @@ import cors from 'cors';
 const app = express();
 const PORT = 3001;
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(bodyParser.json());
 
 // Set up CORS middleware for all routes
